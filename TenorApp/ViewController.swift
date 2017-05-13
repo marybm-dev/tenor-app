3b4e970084af6711d2782488c0e294d7488ba70c
Mary <mlmartinez85@gmail.com>
2017-05-13 13:58:13 -0700

    Filter existing movies

--- TenorApp/ViewController.swift
+++ TenorApp/ViewController.swift
@@ -12,23 +12,64 @@ class ViewController: UIViewController {
 
     @IBOutlet weak var tableView: UITableView!
     
-    var movies: [Movie]! {
+    var movies = [Movie]() {
         didSet {
             self.tableView.reloadData()
         }
     }
     
-    var endPoint: MovieEndPoint!
+    var endPoint: MovieEndPoint! {
+        didSet {
+            self.loadData()
+        }
+    }
+    let segmentControl = UISegmentedControl()
+    var segmentBarItem: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        MovieClient.movies(endPoint: .criticRatings) { [weak self] (movies: [Movie]?, error: Error?) in
+        self.initSetmentControl()
+        self.endPoint = .userRatings
+    }
+    
+    func initSetmentControl() {
+        segmentControl.frame = CGRect(x: 0, y: 0, width: 180, height: 30.0)
+        segmentControl.insertSegment(withTitle: "User Ratings", at: 0, animated: true)
+        segmentControl.insertSegment(withTitle: "Critic Ratings", at: 1, animated: true)
+        segmentControl.selectedSegmentIndex = 0
+        segmentControl.addTarget(self, action: #selector(segmentControlAction(segmentControl:)), for: .valueChanged)
+        segmentBarItem = UIBarButtonItem(customView: segmentControl)
+        self.navigationItem.rightBarButtonItem = segmentBarItem
+    }
+
+    func segmentControlAction(segmentControl: UISegmentedControl) {
+        self.endPoint = segmentControl.selectedSegmentIndex == 0 ? .userRatings : .criticRatings
+    }
+    
+    func loadData() {
+        MovieClient.movies(endPoint: self.endPoint) { [weak self] (endPointMovies: [Movie]?, error: Error?) in
             
-            if let theMovies = movies {
-                self?.movies = theMovies
-            
+            if let theMovies = endPointMovies {
+                let oldMovies = self?.movies
+                var newMovies = [Movie]()
                 
+                // retain the existing Movies and add the new new ones
+                for movie in theMovies {
+                    if (oldMovies?.contains(where: {
+                        if $0.id == movie.id {
+                            newMovies.append($0)
+                        }
+                        return $0.id == movie.id
+                        
+                    }))! { } else {
+
+                        newMovies.append(movie)
+                    }
+                }
+                
+                self?.movies = newMovies
+
             } else {
                 // display alert dialog that says the data was not found
                 let alertController = UIAlertController(title: "Uh oh. Something went wrong", message: "Couldn't get movie data.", preferredStyle: UIAlertControllerStyle.alert)
@@ -39,9 +80,7 @@ class ViewController: UIViewController {
                 self?.present(alertController, animated: true, completion: nil)
             }
         }
-        
     }
-
 }
 
 extension ViewController: UITableViewDelegate, UITableViewDataSource {
